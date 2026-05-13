e358ae9853e1707ffaae229c346386ba5a7ac268
Louie Camacho <louiecamacho@gmail.com>
2026-05-13 14:44:25 -0500

    first init

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <main>
+      <h1>History and Timeline</h1>
+      <nav>navbar of decades here</nav>
+      <div className="container">
+        {/* decades */}
+          {/* events */}
+            
+      </div>
+    </main>
+  )
+}
+
+export default App
